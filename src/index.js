5840728bd70481f71b988d50e917e548e89ea3c0
Talwinder Singh <singhtalwinder790@gmail.com>
2021-03-23 19:25:47 +0530

    Add connection to db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+const { MongoClient } = require("mongodb");
 const app = require("./server");
+const userDAO = require("../dao/userDAO");
 
-app.listen(process.env.PORT, () => {
-  console.log(`http://localhost:${process.env.PORT}`);
-});
+MongoClient.connect(process.env.TASK_DB_URI, { useUnifiedTopology: true })
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    await userDAO.injectDB(client);
+
+    app.listen(process.env.PORT, () => {
+      console.log(`http://localhost:${process.env.PORT}`);
+    });
+  });
