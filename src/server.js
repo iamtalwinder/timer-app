2dba6aca6ca9e6ec46b55682c2d6370e568d88df
Talwinder Singh <singhtalwinder790@gmail.com>
2021-03-24 08:26:35 +0530

    Add user routes in server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const users = require("../api/users.route");
 
 const app = express();
 app.use(express.json());
 
+app.use("/api/v1/user", users);
 app.use("*", (req, res) => res.status(404).send({ error: "not found" }));
 
 module.exports = app;
