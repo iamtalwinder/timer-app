e6444d30b37ac94db54bbbe0d794318cebcfc938
Talwinder Singh <singhtalwinder790@gmail.com>
2021-03-24 09:21:27 +0530

    Change userDAO to UsersDAO

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 const app = require("./server");
-const userDAO = require("../dao/userDAO");
+const UsersDAO = require("../dao/usersDAO");
 
 MongoClient.connect(process.env.TASK_DB_URI, { useUnifiedTopology: true })
   .catch((err) => {
@@ -8,7 +8,7 @@ MongoClient.connect(process.env.TASK_DB_URI, { useUnifiedTopology: true })
     process.exit(1);
   })
   .then(async (client) => {
-    await userDAO.injectDB(client);
+    await UsersDAO.injectDB(client);
 
     app.listen(process.env.PORT, () => {
       console.log(`http://localhost:${process.env.PORT}`);
