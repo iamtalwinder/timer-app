a55a3fb284598c69cc7f119420433b8acc33f217
Talwinder Singh <singhtalwinder790@gmail.com>
2021-03-24 10:03:49 +0530

    Add user-management tests

---
+++ test/user-management.test.js
@@ -0,0 +1,28 @@
+const UsersDAO = require("../dao/usersDAO");
+
+const testUser = {
+  name: "Test",
+  email: "test@test.com",
+  password: "test",
+};
+
+describe("User Management", () => {
+  beforeAll(async () => {
+    await UsersDAO.injectDB(global.taskClient);
+  });
+
+  afterAll(async () => {
+    await UsersDAO.deleteUser(testUser.email);
+  });
+
+  test("Can add a new user to the database", async () => {
+    const actual = await UsersDAO.addUser(testUser);
+    expect(actual.success).toBeTruthy();
+    expect(actual.error).toBeUndefined();
+
+    const user = await UsersDAO.getUser(testUser.email);
+
+    delete user._id;
+    expect(user).toEqual(testUser);
+  });
+});
