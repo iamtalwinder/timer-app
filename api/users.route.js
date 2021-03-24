651786289ca85699dbdac06c9017bda83d5260f0
Talwinder Singh <singhtalwinder790@gmail.com>
2021-03-24 09:18:02 +0530

    Change userCtrl to UsersController

--- api/users.route.js
+++ api/users.route.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
-const usersCtrl = require("./users.controller");
+const UsersController = require("./users.controller");
 
 const router = new Router();
 
-router.post("/register", usersCtrl.register);
-router.post("/login", usersCtrl.login);
+console.log(UsersController);
+
+router.post("/register", UsersController.register);
+router.post("/login", UsersController.login);
 
 module.exports = router;
