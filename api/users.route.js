277f6cead9e340a614c5039816e3187142a29d55
Talwinder Singh <singhtalwinder790@gmail.com>
2021-03-25 13:43:13 +0530

    Remove unnecessary console.log

--- api/users.route.js
+++ api/users.route.js
@@ -3,8 +3,6 @@ const UsersController = require("./users.controller");
 
 const router = new Router();
 
-console.log(UsersController);
-
 router.post("/register", UsersController.register);
 router.post("/login", UsersController.login);
 
