5c76b31819ed41fb67a5dc7c171804931b44423f
Talwinder Singh <singhtalwinder790@gmail.com>
2021-03-26 00:21:02 +0530

    Add DashboardMenu component

---
+++ app/src/components/DashboardMenu.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { Menu, Appbar } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+
+export default function DashboardMenu() {
+  const [visible, setVisible] = useState(false);
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+
+  const navigation = useNavigation();
+
+  return (
+    <Menu
+      visible={visible}
+      onDismiss={closeMenu}
+      anchor={<Appbar.Action icon="menu" color="white" onPress={openMenu} />}
+    >
+      <Menu.Item
+        onPress={() => {
+          navigation.reset({
+            index: 0,
+            routes: [{ name: "HomeScreen" }],
+          });
+        }}
+        title="Log Out"
+      />
+    </Menu>
+  );
+}
--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Paragraph } from "./Paragraph";
 export { default as Button } from "./Button";
 export { default as BackButton } from "./BackButton";
 export { default as TextInput } from "./TextInput";
+export { default as DashboardMenu } from "./DashboardMenu";
