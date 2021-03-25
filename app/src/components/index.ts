426a76b8a003f0d8a18124fa2e09aef2f38516b7
Talwinder Singh <singhtalwinder790@gmail.com>
2021-03-25 06:06:42 +0530

    Add BackButton component

--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from "./Header";
 export { default as Background } from "./Background";
 export { default as Paragraph } from "./Paragraph";
 export { default as Button } from "./Button";
+export { default as BackButton } from "./BackButton";
