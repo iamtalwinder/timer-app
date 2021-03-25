d8bc689903193d76f375df4219ebd030080064fa
Talwinder Singh <singhtalwinder790@gmail.com>
2021-03-25 10:49:20 +0530

    Add navigation

--- app/App.tsx
+++ app/App.tsx
@@ -1,16 +1,33 @@
 import React from "react";
-import { Provider as PaperProvider, Button } from "react-native-paper";
+import { Provider as PaperProvider } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+  HomeScreen,
+  LoginScreen,
+  SignupScreen,
+  DashboardScreen,
+} from "./src/screens";
+import { theme } from "./src/core/theme";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <PaperProvider>
-      <Button
-        icon="camera"
-        mode="contained"
-        onPress={() => console.log("Pressed")}
-      >
-        Press me
-      </Button>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Homecreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="SignupScreen" component={SignupScreen} />
+          <Stack.Screen name="DashboardScreen" component={DashboardScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
