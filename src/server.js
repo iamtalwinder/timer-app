1aade9493b8fbce5a203d4026666af9148d4945c
Talwinder Singh <singhtalwinder790@gmail.com>
2021-03-25 13:40:03 +0530

    Add cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const users = require("../api/users.route");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/user", users);
