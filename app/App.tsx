70ae8a19e5049886fc290331eb2b1a72c04ecb1a
Talwinder Singh <singhtalwinder790@gmail.com>
2021-03-26 11:06:54 +0530

    Change initial route

--- app/App.tsx
+++ app/App.tsx
@@ -32,7 +32,7 @@ export default function App() {
           >
             <NavigationContainer>
               <Stack.Navigator
-                initialRouteName="DashboardScreen"
+                initialRouteName="StartScreen"
                 screenOptions={{
                   headerShown: false,
                   headerStyle: {
