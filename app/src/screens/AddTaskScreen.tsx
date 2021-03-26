362fc8c946c02ab0bdfe449ccffd5b623438a5eb
Talwinder Singh <singhtalwinder790@gmail.com>
2021-03-26 09:24:33 +0530

    Add AddTask functionality

--- app/src/screens/AddTaskScreen.tsx
+++ app/src/screens/AddTaskScreen.tsx
@@ -1,10 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useReducer, useContext } from "react";
 import { View, StyleSheet } from "react-native";
-import { Background, TextInput } from "../components";
+import { Background, TextInput, TimePicker, Button } from "../components";
+import { FormInput } from "./types";
+import TimeConverter, { Time } from "../lib/timeConverter";
+import { Context as TasksContext } from "../context/tasks";
 
-export default function AddTaskScreen() {
-  const [title, setTitle] = useState({ value: "", error: "" });
-  const [description, setDescription] = useState({ value: "", error: "" });
+type TimeAction =
+  | { type: "CHANGE_HOURS"; payload: number }
+  | { type: "CHANGE_MINUTES"; payload: number }
+  | { type: "CHANGE_SECONDS"; payload: number };
+
+export default function AddTaskScreen({ navigation }: any) {
+  const [title, setTitle] = useState<FormInput>({ value: "", error: "" });
+  const [description, setDescription] = useState<FormInput>({
+    value: "",
+    error: "",
+  });
+
+  const timeReducer = (state: Time, action: TimeAction): Time => {
+    switch (action.type) {
+      case "CHANGE_HOURS":
+        return { ...state, hours: action.payload };
+
+      case "CHANGE_MINUTES":
+        return TimeConverter.correct({ ...state, minutes: action.payload });
+
+      case "CHANGE_SECONDS":
+        return TimeConverter.correct({ ...state, seconds: action.payload });
+
+      default:
+        return state;
+    }
+  };
+
+  const [time, timeDispatch] = useReducer(
+    timeReducer,
+    {
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    },
+    () => TimeConverter.toHHMMSS(3)
+  );
+
+  const { dispatch: tasksDispatch } = useContext(TasksContext);
+
+  const addTask = () => {
+    tasksDispatch({
+      type: "ADD_TASK",
+      payload: { title: title.value, description: description.value, time },
+    });
+    navigation.goBack();
+  };
 
   return (
     <Background>
@@ -32,6 +79,12 @@ export default function AddTaskScreen() {
           errorText={description.error}
           autoCapitalize="none"
         />
+
+        <TimePicker time={time} timeDispatch={timeDispatch} />
+
+        <Button mode="contained" onPress={addTask}>
+          Add
+        </Button>
       </View>
     </Background>
   );
