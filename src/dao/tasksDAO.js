6dd00c0afa1abe91deb538804faac011c0157ba1
Talwinder Singh <singhtalwinder790@gmail.com>
2021-04-10 06:07:01 +0530

    Add tasksDAO

---
+++ src/dao/tasksDAO.js
@@ -0,0 +1,84 @@
+let tasks;
+
+module.exports = class TasksDAO {
+  static async injectDB(conn) {
+    if (tasks) return;
+
+    try {
+      tasks = await conn.db(process.env.TASK_NS).collection("tasks");
+    } catch (e) {
+      console.error(`Unable to establish collection handles in tasksDAO: ${e}`);
+    }
+  }
+
+  /**
+   * Finds a task in 'tasks' collection
+   * @param {string} taskId - The _id of the task in the `tasks` collection
+   * @returns {Object | null} Returns either a single task or nothing
+   */
+
+  static async getTask(taskId) {
+    return await tasks.findOne({ _id: taskId });
+  }
+
+  /**
+   * Add a task to 'tasks' collection
+   * @param {TaskInfo} taskInfo - The information of the task to add
+   * @returns {DAOResponse} - Returns either a "success" or an "error" Object
+   */
+
+  static async addTask(taskInfo) {
+    try {
+      await tasks.insertOne(taskInfo);
+      return { success: true };
+    } catch (e) {
+      console.error(`Error occured while adding new task, ${e}`);
+      return { error: e };
+    }
+  }
+
+  /**
+   * Delete a task from the 'tasks' collection
+   * @param {string} taskId - The _id of the task in the `tasks` collection
+   * @returns {DAOResponse} - Returns either a "success" or an "error" Object
+   */
+
+  static async deleteTask(taskId) {
+    try {
+      await tasks.deleteOne({ _id: taskId });
+
+      if (!(await this.getTask(taskId))) {
+        return { success: true };
+      } else {
+        console.error("Deletion unsuccessful");
+        return { error: "Deletion unsuccessful" };
+      }
+    } catch (e) {
+      console.log(`Error occured while,${e}`);
+      return { error: e };
+    }
+  }
+};
+
+/**
+ * Parameter passed to addTask method
+ * @typedef TaskInfo
+ * @property {string} title
+ * @property {string} description
+ * @property {Time} time
+ */
+
+/**
+ * Time subdocument
+ * @typedef Time
+ * @property {number} hours
+ * @property {numeber} minutes
+ * @property {number} seconds
+ */
+
+/**
+ * Success/Error return object
+ * @typedef DAOResponse
+ * @property {boolean} [success] - Success
+ * @property {string} [error] - Error
+ */
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require("mongodb");
 const app = require("./server");
 const UsersDAO = require("./dao/usersDAO");
+const TasksDAO = require("./dao/tasksDAO");
 
 MongoClient.connect(process.env.TASK_DB_URI, { useUnifiedTopology: true })
   .catch((err) => {
@@ -9,6 +10,7 @@ MongoClient.connect(process.env.TASK_DB_URI, { useUnifiedTopology: true })
   })
   .then(async (client) => {
     await UsersDAO.injectDB(client);
+    await TasksDAO.injectDB(client);
 
     app.listen(process.env.PORT, () => {
       console.log(`http://localhost:${process.env.PORT}`);
