f75d47747c51a29c3ea44ee11230b3fc07aba45f
Talwinder Singh <singhtalwinder790@gmail.com>
2021-04-17 10:06:23 +0530

    Add update and delete task routes

--- src/api/tasks.controller.js
+++ src/api/tasks.controller.js
@@ -14,9 +14,47 @@ module.exports = class TasksController {
 
       if (insertResult.insertedCount === 1) {
         const task = await TasksDAO.getTask(insertResult.insertedId);
-        res.status(200).send({ task, msg: "Task added" });
+        res.status(201).send({ task, msg: "Task added" });
       } else {
         res.status(500).send({ msg: "Unable to add task" });
+      }
+    } catch (e) {
+      console.error(e);
+      res.status(500).send({ msg: "Internal server error" });
+    }
+  }
+
+  static async apiUpdateTask(req, res) {
+    try {
+      const { taskId, title, description, time } = req.body;
+
+      await TasksDAO.updateTask(
+        taskId,
+        req.user._id,
+        title,
+        description,
+        time,
+        new Date()
+      );
+
+      const updatedTaskFromDB = await TasksDAO.getTask(taskId);
+      res.status(200).send({ msg: "Task updated", updatedTaskFromDB });
+    } catch (e) {
+      console.error(e);
+      res.status(500).send({ msg: "Internal server error" });
+    }
+  }
+
+  static async apiDeleteTask(req, res) {
+    try {
+      const { taskId } = req.body;
+
+      const deleteResult = await TasksDAO.deleteTask(taskId);
+
+      if (deleteResult.success) {
+        res.status(200).send({ msg: "Task deleted" });
+      } else {
+        res.status(500).send({ mag: deleteResult.error });
       }
     } catch (e) {
       console.error(e);
--- src/api/tasks.route.js
+++ src/api/tasks.route.js
@@ -1,11 +1,17 @@
 const { Router } = require("express");
 const TasksController = require("./tasks.controller");
 const { validator, auth } = require("./middlewares");
-const { addTaskSchema } = require("./tasks.controller.schema");
+const {
+  addTaskSchema,
+  updateTaskSchema,
+  deleteTaskSchema,
+} = require("./tasks.controller.schema");
 
 const router = new Router();
 router.use(auth);
 
 router.post("/", validator(addTaskSchema), TasksController.apiAddTask);
+router.put("/", validator(updateTaskSchema), TasksController.apiUpdateTask);
+router.delete("/", validator(deleteTaskSchema), TasksController.apiDeleteTask);
 
 module.exports = router;
